bcabd7c3133c393f49844802b6b1b222ada14b68
HenryPB123 <mahe2di@gmail.com>
2023-02-11 19:38:37 -0500

    Terminando con Bootstrap

--- src/components/form/RecipeCreate.jsx
+++ src/components/form/RecipeCreate.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { addRecipes, getDiets } from "../../redux/actions/actions";
-import "./create.css";
+import "../home/home.scss";
 
 export default function RecipeCreate() {
   const dispatch = useDispatch();
@@ -116,20 +116,19 @@ export default function RecipeCreate() {
   }
 
   return (
-    <div className="container3">
-      <div className="form">
-        <form className="form-container" onSubmit={(e) => handleSubmit(e)}>
-          <h2 className="title">CREATE RECIPE</h2>
-          <div className="create-title">
-            <div>
-              <div className="titles">
+    <div className="container-fluid  bg-black bg-opacity-75 p-5 text-warning   text-center mt-5">
+      <div className="h1 text-center mb-5">CREATE RECIPE</div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="container">
+          <div className=" row bg-dark p-3 mb-5">
+            <div className="col-6">
+              <div className="container text-start ms-5 ">
                 <label htmlFor="1">
                   <strong>Name:</strong>
-                </label>
+                </label>{" "}
                 <br />
-              </div>
-              <div className="input">
                 <input
+                  className="border border-warning rounded m-1 col-8"
                   key="name"
                   id="1"
                   placeholder="Name"
@@ -142,17 +141,14 @@ export default function RecipeCreate() {
                 />
                 {errorName && <p>{errorName}</p>}
               </div>
-            </div>
 
-            <div>
-              <div className="titles">
+              <div className="container text-start ms-5">
                 <label htmlFor="2">
                   <strong>Image:</strong>
                 </label>
                 <br />
-              </div>
-              <div className="image">
                 <input
+                  className="border border-warning rounded m-1 col-8"
                   key="image"
                   placeholder="https://example.com"
                   id="2"
@@ -164,16 +160,14 @@ export default function RecipeCreate() {
                 />
                 {errorImage && <p>{errorImage}</p>}
               </div>
-            </div>
-            <div>
-              <div className="titles">
+
+              <div className="container text-start ms-5 ">
                 <label htmlFor="3">
                   <strong>Summary:</strong>
                 </label>
                 <br />
-              </div>
-              <div className="summary">
                 <textarea
+                  className="border border-warning rounded m-1 col-8"
                   key="summary"
                   id="3"
                   placeholder="Summary..."
@@ -186,106 +180,122 @@ export default function RecipeCreate() {
                 {errorSummary && <p>{errorSummary}</p>}
               </div>
             </div>
-            <div className="titles">
-              <label htmlFor="4">
-                <strong>HealthScore:</strong>
-              </label>
-              <br />
-              <div className="score1">
+
+            <div className="col-6">
+              <div className="container text-start ms-5 ">
+                <label htmlFor="4">
+                  <strong>HealthScore:</strong>
+                </label>
+                <br />
                 <input
+                  className="border border-warning rounded m-1 col-8"
                   key="healthScore"
-                  id="3"
+                  id="4"
                   placeholder="'Score'"
                   type="number"
                   value={input.healthScore}
                   name="healthScore"
                   onChange={(e) => handleChange(e)}
                 />
+                {errorHealth && <p>{errorHealth}</p>}
               </div>
-              {errorHealth && <p>{errorHealth}</p>}
-            </div>
-            <div>
-              <div className="titles">
+              <div className="container text-start ms-5 ">
                 <label htmlFor="5">
                   <strong>Steps:</strong>
                 </label>
                 <br />
-              </div>
-              <div className="steps">
                 <input
+                  className="border border-warning rounded m-1 col-8"
                   key="steps"
                   id="5"
                   placeholder="steps..."
                   value={text}
                   name="steps"
                   onChange={(e) => handleSteps(e)}
-                ></input>
-                {<button onClick={() => handleClick()}>Add step</button>}
+                />
+                <button
+                  className="btn btn-warning btn-sm ms-2"
+                  onClick={() => handleClick()}
+                >
+                  Add step
+                </button>
                 {errorSteps && <p>{errorSteps}</p>}
-
                 {input.steps?.map((p, i) => (
-                  <div>
+                  <div key={i}>
                     {`${i + 1} - ${p.step}`}
                     <button onClick={() => deleteStep(p.step)}>X</button>
                   </div>
                 ))}
               </div>
-            </div>
-            <div htmlFor="6" className="dietas">
-              <select
-                key="dieta"
-                value="recipes"
-                onChange={(e) => handleSelect(e)}
-              >
-                {dietas?.map((e) => (
-                  <option value={e}>{e}</option>
+              <div className="container text-start ms-5 ">
+                <label htmlFor="6">
+                  <strong>Diets Types:</strong>{" "}
+                </label>
+                <br />
+                <select
+                  className="border border-warning rounded m-1 col-8"
+                  id="6"
+                  key="dieta"
+                  value="recipes"
+                  onChange={(e) => handleSelect(e)}
+                >
+                  {dietas?.map((e, i) => (
+                    <option key={i} value={e}>
+                      {e}
+                    </option>
+                  ))}
+                </select>
+                <div>Select one or more diets</div>
+              </div>
+              {input.dieta.length > 0 &&
+                input.dieta.map((diet, i) => (
+                  <div key={i}>
+                    {diet}
+                    <button
+                      onClick={() => {
+                        deleteDiet(diet);
+                      }}
+                    >
+                      X
+                    </button>
+                  </div>
                 ))}
-              </select>
             </div>
-            {input.dieta.length > 0 &&
-              input.dieta.map((diet, i) => (
-                <div key={i}>
-                  {diet}
-                  <button
-                    onClick={() => {
-                      deleteDiet(diet);
-                    }}
-                  >
-                    X
-                  </button>
-                </div>
-              ))}
-
-            <button
-              className="button-create"
-              disabled={
-                errorName.length !== 0 ||
-                errorSummary.length !== 0 ||
-                errorSteps.length !== 0 ||
-                errorHealth.length !== 0 ||
-                errorImage.length !== 0
-              }
-              type="submit"
-              onClick={() => {
-                dispatch(addRecipes(input));
-                alert("Recipe Created Successfully");
-                setInput({
-                  name: "",
-                  image: "",
-                  summary: "",
-                  healthScore: 0,
-                  steps: [],
-                  dieta: [],
-                });
-              }}
-            >
-              Create recipe
-            </button>
-
-            <Link to={"/Home"}>Back</Link>
           </div>
-        </form>
-      </div>
+        </div>
+
+        <div className="container">
+          <button
+            className="btn btn-warning me-5 col-3"
+            disabled={
+              errorName.length !== 0 ||
+              errorSummary.length !== 0 ||
+              errorSteps.length !== 0 ||
+              errorHealth.length !== 0 ||
+              errorImage.length !== 0
+            }
+            type="submit"
+            onClick={() => {
+              dispatch(addRecipes(input));
+              alert("Recipe Created Successfully");
+              setInput({
+                name: "",
+                image: "",
+                summary: "",
+                healthScore: 0,
+                steps: [],
+                dieta: [],
+              });
+            }}
+          >
+            Create recipe
+          </button>
+
+          <Link to={"/Home"}>
+            <button className="btn btn-warning ms-5 col-3"> Back</button>
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
